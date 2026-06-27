f1abfc492ef30f3b63325dd7d4d69eb9fc734984
Giuseppe Papa <papa57886@gmail.com>
2026-06-27 09:32:15 -0400

    Otro push bro
    Agrege el login para  que lo veas y amodode la vista

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-// Importamos los componentes del Administrador
+
 import AdminNav from './components/admin/AdminNav';
 import HomeScreen from './components/admin/HomeScreen';
 import AdminPropietarios from './components/admin/AdminPropietarios';
 import AdminPagos from './components/admin/AdminPagos';
 import AdminBloqueados from './components/admin/AdminBloqueados'; 
 import ManualAccess from './components/admin/ManualAccess';
-
-// Importamos el componente de Usuario
+import Auth from './components/Auth';
 import DashboardUser from './components/user/DashboardUser';
 
 function App() {
   return (
     <Router>
-      <div className="app-container" style={{ display: 'flex', flexDirection: 'column', gap: '50px' }}>
+      <div>
         
-        {/* SECCIÓN DE ADMINISTRADOR (ARRIBA) - CON RUTAS ACTIVAS */}
-        <div className="admin-workspace-section" style={{ borderBottom: '3px dashed #222', paddingBottom: '40px' }}>
-          <div style={{ color: '#666', fontSize: '12px', letterSpacing: '2px', padding: '10px 40px', fontWeight: 'bold' }}>
-            ENTORNO DE DESARROLLO: MÓDULO ADMINISTRATIVO (NAVEGACIÓN ACTIVA)
-          </div>
-          
+        {/* admin */}
+        <div>
+      
           <Routes>
-            {/* Si entras a la raíz "/" te redirige automáticamente a las torres o al home del admin */}
+
++
             <Route path="/" element={<Navigate to="/admin/home" replace />} />
 
-            {/* Configuración de rutas anidadas para el Administrador */}
+
+
             <Route path="/admin" element={<AdminNav />}>
               <Route index element={<Navigate to="home" replace />} />
               <Route path="home" element={<HomeScreen />} />
@@ -37,14 +35,15 @@ function App() {
               <Route path="manual-access" element={<ManualAccess/>} />
             </Route>
           </Routes>
+
         </div>
 
-        {/* SECCIÓN DE USUARIO (ABAJO) - ESTÁTICA PARA MONITOREO */}
-        <div className="user-workspace-section" style={{ paddingTop: '20px' }}>
-          <div style={{ color: '#666', fontSize: '12px', letterSpacing: '2px', padding: '10px 40px', fontWeight: 'bold' }}>
-            ENTORNO DE DESARROLLO: MÓDULO DE USUARIO (FIJO)
-          </div>
+  {/* user */}
+        <div>
+  
           <DashboardUser />
+          <Auth/>
+
         </div>
 
       </div>
