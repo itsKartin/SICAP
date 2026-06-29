8a3bfd371881a4fe0f74e3d9037941100ce84b45
kartin <204815043+itsKartin@users.noreply.github.com>
2026-06-29 03:44:30 -0400

    owners routes and mewdue route

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,11 +2,49 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import date
 
-class LoginRequest(BaseModel):
+#Auth route
+class LoginRequest(BaseModel): #Login /login
     email:str
     password:str
 
-class OwnerCreate(BaseModel):
+#Owners routes
+
+class PaymentUpload(BaseModel):
+    amount_bs: float
+    receipt: str
+
+class PaymentHistoryItem(BaseModel):
+    id: int
+    amount_bs: float
+    payment_date: date
+    status: str
+    amount_usd: Optional[float] = None
+    exchange_rate: Optional[float] = None
+
+    class Config:
+        from_attributes = True
+
+class DueOut(BaseModel):
+    id: int
+    amount_usd: float
+    month: date
+    due_date: date
+    status: str
+
+    class Config:
+        from_attributes = True
+
+        
+class DebtSummary(BaseModel):
+    pending_dues: list[DueOut]
+    total_debt_usd: float
+    total_debt_bs: float
+    pending_count: int
+    exchange_rate: float
+
+#Admin routes
+
+class OwnerCreate(BaseModel): #Registro de propietario /newowner 
     firstname: str
     lastname: str
     email: str
@@ -17,7 +55,7 @@ class OwnerCreate(BaseModel):
     tower: str
     passw: str
 
-class OwnerOut(BaseModel):
+class OwnerOut(BaseModel): #Lista de usuario /ownerlist 
     id: int
     first_name: str
     last_name: str
@@ -33,7 +71,7 @@ class OwnerOut(BaseModel):
     class Config:
         from_attributes = True
 
-class PaymentOut(BaseModel):
+class PaymentOut(BaseModel): #Lista de pagos /pending-payments 
     id: int
     owner_id: int
     amount_bs: float
@@ -43,7 +81,7 @@ class PaymentOut(BaseModel):
     owner_name: str
     owner_apartment: str
 
-class VerifyPaymentResponse(BaseModel):
+class VerifyPaymentResponse(BaseModel):  #Verificacion de pagos /payment-verification/{payment_id}
     message: str
     payment_id: int
     owner_id: int
@@ -52,7 +90,10 @@ class VerifyPaymentResponse(BaseModel):
     exchange_rate: float
     dues_covered: list[int]
     owner_unblocked: bool
-    owner_blocked: bool
     pending_dues_count: int
     pending_debt_usd: float
     overpayment_usd: float
+
+class GenerateDuesRequest(BaseModel):
+    amount_usd: float
+    due_date: date
